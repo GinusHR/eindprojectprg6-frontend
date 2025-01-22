6ae90fd268f30e4d821ea27afa5982e2b9cfb3b2
GinusHR <145678936+GinusHR@users.noreply.github.com>
2025-01-22 12:09:10 +0100

    css changes2

--- src/components/DeleteButton.jsx
+++ src/components/DeleteButton.jsx
@@ -1,5 +1,5 @@
 function DeleteButton() {
-
+    
 }
 
 export default DeleteButton;
--- src/components/form/ObjectComponent.jsx
+++ src/components/form/ObjectComponent.jsx
@@ -5,10 +5,10 @@ function les2Component({transformer}) {
         <div className="flex flex-row flex-wrap w-11/12 ">
             {transformer.map((transformer) => (
                 <article key={transformer.id} className="w-[20vw] h-[24vh] m-2 rounded-lg p-3">
-                    <h2 className="text-red-700">{transformer.name}</h2>
+                    <h2>{transformer.name}</h2>
                     <h3 className="text-gray-800">{transformer.faction}</h3>
                     <p className="text-gray-500">{transformer.description}</p>
-                    <Link className="text-purple-700 font-normal" to={`/transformers/${transformer.id}`}>edit</Link>
+                    <Link className="detailLink" to={`/transformers/${transformer.id}`}>Edit</Link>
                 </article>
             ))
             }
